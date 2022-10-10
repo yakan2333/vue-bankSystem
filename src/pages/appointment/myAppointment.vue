5907d62d5a6539390aea120f26b13d309c549fa5
jrq <2732885038@qq.com>
2022-10-10 08:43:04 +0800

    我的预约服务更新

--- src/pages/appointment/myAppointment.vue
+++ src/pages/appointment/myAppointment.vue
@@ -1,12 +1,31 @@
-<template></template>
+<template>
+    <div>
+        <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="id" label="网点编号" width="180">
+            </el-table-column>
+            <el-table-column prop="name" label="网点名" width="180">
+            </el-table-column>
+            <el-table-column prop="time" label="预约时间段" width="180">
+            </el-table-column>
+            <el-table-column prop="createTime" label="创建时间">
+            </el-table-column>
+            <el-table-column prop="num" label="排队序号">
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
 
 <script>
+
 import { thisTypeAnnotation } from '@babel/types';
+
 import axios from 'axios'
 export default {
 
     data() {
-
+        return {
+            tableData: [],
+        };
     },
 
     mounted() {
@@ -21,9 +40,26 @@ export default {
 
             axios.get("/myAppointment", {
                 params: { userId: p.id }
-            }).then((resp) => {
-                console.log(resp);
+            }).then((response) => {
+                let list = response.data.data.data;
+                console.log(list);
+                this.showList(list);
             });
+        },
+
+        //将信息展示在表格上
+        showList(result) {
+
+            this.tableData = [];
+            for (let i = 0; i < result.length; i++) {
+                this.tableData.push({
+                    id: result[i].mechanismId,
+                    name: result[i].mechanismName,
+                    time: result[i].time,
+                    createTime: result[i].createTime,
+                    num: result[i].num,
+                });
+            }
         },
     }
 };
